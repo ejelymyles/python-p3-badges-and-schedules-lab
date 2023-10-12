69293c9ec9c8abe4833bb6630a1f5b2a9d5a7a04
Ej Myles <ejelymyles@gmail.com>
2023-10-12 13:49:13 -0700

    finish lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms = list(range(1,9))
+    room_assignments = []
 
+    for name in names:
+       if rooms:
+        room = rooms.pop(0)
+        assignment = f"Hello, {name}! You'll be assigned to room {room}!"
+        room_assignments.append(assignment)
+       else:
+        break
+    return room_assignments
+    
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge in badge_messages:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
